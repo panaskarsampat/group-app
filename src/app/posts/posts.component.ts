91bd293c098edec63a10f5a66a6f52653664d6e5
Sampat Panaskar <sampat.panaskar@ivaluategt.com>
2018-06-28 06:37:06 -0400

    Done Technologies Post Details.

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { PostsModels } from '../posts/posts-models';
+import { PostsService } from '../posts/posts.service';
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -7,9 +10,31 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
   @Input() techId:number;
-  constructor() { }
+  articaleslist:PostsModels[];
+  startPage : Number;
+  paginationLimit:Number;
+
+  constructor(private postsService:PostsService) { }
 
   ngOnInit() {
+    this.getArticalesByTechId();
   }
 
+  getArticalesByTechId(){
+    this.postsService.getArticales().subscribe(
+      art => {        
+        this.articaleslist = art.filter(a=>a.TechnologiesId==this.techId);    
+        this.startPage = 0;
+        this.paginationLimit = 3;    
+      },
+      err => {
+        console.log(err);        
+      }
+    );
+  }
+
+  showMoreItems()
+  {     
+    this.paginationLimit = Number(this.paginationLimit) + 3;        
+  }
 }
