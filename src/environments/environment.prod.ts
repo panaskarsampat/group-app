3d651d3ce2fbea9ed97f810ed59c68b5b7f965a2
Sampat Panaskar <sampat.panaskar@ivaluategt.com>
2018-07-30 10:52:30 -0400

    Rename live server api in enviroment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //   ROOT_URL:"http://arnvikgroupapi.arnvikgroup.com/api/"
-   ROOT_URL:"http://localhost:57354/api/"
+   ROOT_URL:"http://arnvikgroupapi.arnvikgroup.com/api/"
+   //ROOT_URL:"http://localhost:57354/api/"
 };
